1f4148f88e1d0fa8957bea69cdc4eac25900fcd0
MstyslavSoroka <mstyslav2010@gmail.com>
2026-03-04 21:38:45 +0000

    header responsive

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -4,10 +4,20 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 function AboutSection() {
   return (
     <>
-      <section className="bg-zinc-800 text-zinc-200">
-        <div className="py-20 px-4 lg:container mx-auto">
-          <h1 className="text-4xl font-bold">Hey, I'm Mstyslav</h1>
-          <p>Based in Aberdeen, Scotland</p>
+      <section className="bg-zinc-800 text-zinc-200 flex items-center justify-center">
+        <div className="flex flex-col-reverse md:flex-row container mx-auto items-center justify-center gap-10">
+          <div className="py-20 px-4 lg:container mx-auto">
+            <h1 className="text-4xl font-bold text-5xl ">
+              Hey, I'm{' '}
+              <span className="bg-gradient-to-tl from-[#E8E8E8] via-[#C0C0C0] to-[#A0A0A0] bg-clip-text text-transparent pr-4  ">
+                Mstyslav
+              </span>
+              <span className="inline-flex cursor-pointer  hover:animate-wiggle">
+                👋
+              </span>
+            </h1>
+            <p>Based in Aberdeen, Scotland</p>
+          </div>
           <Map
             initialViewState={{
               longitude: -2.09,
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -34,9 +34,9 @@ function Header() {
   }, [active]);
 
   return (
-    <header className="flex bg-zinc-800 text-zinc-200 justify-center">
+    <header className="flex bg-zinc-800 text-zinc-200 justify-center px-5">
       <nav className="flex items-center w-full py-4 lg:container justify-between">
-        <div className="font-mono relative h-[64px] scale-75 sm:scale-100 flex items-center rounded-full p-2">
+        <div className="flex font-mono relative h-[64px] scale-75 sm:scale-100 flex items-center rounded-full p-2">
           <div className="absolute inset-0 rounded-full bg-linear-to-b from-neutral-700 to-neutral-800 shadow-[0_8px_32px_rgba(0,0,0,0.6)]" />
           <div className="absolute inset-0.75 rounded-full bg-linear-to-b from-neutral-800 to-neutral-900" />
           <div className="absolute inset-2 rounded-full bg-white/5 backdrop-blur-sm" />
@@ -48,41 +48,43 @@ function Header() {
           </span>
         </div>
 
-        <div
-          ref={containerRef}
-          className="relative h-[64px] scale-75 sm:scale-100 flex items-center rounded-full p-2"
-        >
-          <div className="absolute inset-0 rounded-full bg-linear-to-b from-neutral-700 to-neutral-800 shadow-[0_8px_32px_rgba(0,0,0,0.6)]" />
-          <div className="absolute inset-0.75 rounded-full bg-linear-to-b from-neutral-800 to-neutral-900" />
-          <div className="absolute inset-2 rounded-full bg-white/5 backdrop-blur-sm" />
-
+        <div className="hidden md:flex md:absolute left-[50%] md:-translate-x-1/2">
           <div
-            className="absolute top-2 h-[48px] rounded-full bg-linear-to-b from-[#E8E8E8] via-[#C0C0C0] to-[#A0A0A0] shadow-[0_2px_8px_rgba(0,0,0,0.4)] transition-all duration-300 ease-out"
-            style={{
-              left: pillStyle.left,
-              width: pillStyle.width,
-            }}
-          />
+            ref={containerRef}
+            className="relative h-[64px] flex items-center rounded-full p-2"
+          >
+            <div className="absolute inset-0 rounded-full bg-linear-to-b from-neutral-700 to-neutral-800 shadow-[0_8px_32px_rgba(0,0,0,0.6)]" />
+            <div className="absolute inset-0.75 rounded-full bg-linear-to-b from-neutral-800 to-neutral-900" />
+            <div className="absolute inset-2 rounded-full bg-white/5 backdrop-blur-sm" />
 
-          {navItems.map((item, i) => (
-            <NavLink key={item.path} to={item.path}>
-              <span
-                ref={(el) => (buttonsRef.current[i] = el)}
-                className="relative z-10 px-6 py-3 rounded-full cursor-pointer inline-block"
-              >
+            <div
+              className="absolute top-2 h-[48px] rounded-full bg-linear-to-b from-[#E8E8E8] via-[#C0C0C0] to-[#A0A0A0] shadow-[0_2px_8px_rgba(0,0,0,0.4)] transition-all duration-300 ease-out"
+              style={{
+                left: pillStyle.left,
+                width: pillStyle.width,
+              }}
+            />
+
+            {navItems.map((item, i) => (
+              <NavLink key={item.path} to={item.path}>
                 <span
-                  className={`text-base font-medium transition-colors ${
-                    i === active ? 'text-neutral-900' : 'text-neutral-300'
-                  }`}
+                  ref={(el) => (buttonsRef.current[i] = el)}
+                  className="relative z-10 px-6 py-3 rounded-full cursor-pointer inline-block"
                 >
-                  {item.label}
+                  <span
+                    className={`text-base font-medium transition-colors ${
+                      i === active ? 'text-neutral-900' : 'text-neutral-300'
+                    }`}
+                  >
+                    {item.label}
+                  </span>
                 </span>
-              </span>
-            </NavLink>
-          ))}
+              </NavLink>
+            ))}
+          </div>
         </div>
 
-        <div className="font-mono relative h-[64px] scale-75 sm:scale-100 flex items-center rounded-full p-2">
+        <div className="font-mono scale-75 sm:scale-100 relative h-[64px] flex items-center rounded-full p-2">
           <div className="absolute inset-0 rounded-full bg-linear-to-b from-neutral-700 to-neutral-800 shadow-[0_8px_32px_rgba(0,0,0,0.6)]" />
           <div className="absolute inset-0.75 rounded-full bg-linear-to-b from-neutral-800 to-neutral-900" />
           <div className="absolute inset-2 rounded-full bg-white/5 backdrop-blur-sm" />
