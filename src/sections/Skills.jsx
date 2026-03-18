c3a9276d7b957e3a30d751ddd89e7ebb03868648
MstyslavSoroka <mstyslav2010@gmail.com>
2026-03-18 17:22:42 +0000

    start skills

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,7 +1,40 @@
 import Header from './Header';
+import {
+  SiReact,
+  SiReactHex,
+  SiHtml5,
+  SiHtml5Hex,
+  SiCss,
+  SiCssHex,
+  SiTailwindcss,
+  SiTailwindcssHex,
+  SiJavascript,
+  SiJavascriptHex,
+  SiGit,
+  SiGitHex,
+  SiLinux,
+  SiLinuxHex,
+} from '@icons-pack/react-simple-icons';
 
 function Skills() {
-  return <></>;
+  const skills = [
+    { Icon: SiReact },
+    { Icon: SiHtml5 },
+    { Icon: SiCss },
+    { Icon: SiTailwindcss },
+    { Icon: SiJavascript },
+    { Icon: SiGit },
+    { Icon: SiLinux },
+  ];
+  return (
+    <>
+      <section className="bg-zinc-950">
+        {skills.map(({ Icon }, i) => (
+          <Icon key={i} className="fill-zinc-200" />
+        ))}
+      </section>
+    </>
+  );
 }
 
 export default Skills;
